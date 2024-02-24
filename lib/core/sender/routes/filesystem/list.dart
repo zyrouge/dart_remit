d9ae445ef690863a3188dbcbc4142e5780f4383c
zyrouge <zyrouge@hotmail.com>
2024-02-24 19:36:04 +0530

    chore: backup

--- lib/core/sender/routes/filesystem/list.dart
+++ lib/core/sender/routes/filesystem/list.dart
@@ -2,10 +2,57 @@ import 'package:http/http.dart' as http;
 import 'package:remit/exports.dart';
 import 'package:shelf/shelf.dart' as shelf;
 
-typedef RemitSenderServerFilesystemListData = ({
-  List<String> files,
-  List<String> folders
+typedef _RouteDataFile = ({
+  String basename,
+  int size,
 });
+
+typedef _RouteDataFolder = ({
+  String basename,
+});
+
+class RemitSenderServerFilesystemRouteData {
+  const RemitSenderServerFilesystemRouteData({
+    required this.files,
+    required this.folders,
+  });
+
+  factory RemitSenderServerFilesystemRouteData.fromJson(
+    final Map<dynamic, dynamic> json,
+  ) =>
+      RemitSenderServerFilesystemRouteData(
+        files: (json[RemitDataKeys.files] as List<dynamic>)
+            .cast<List<dynamic>>()
+            .map(fileFromJson)
+            .toList(),
+        folders: (json[RemitDataKeys.folders] as List<dynamic>)
+            .cast<List<dynamic>>()
+            .map(folderFromJson)
+            .toList(),
+      );
+
+  final List<_RouteDataFile> files;
+  final List<_RouteDataFolder> folders;
+
+  Map<dynamic, dynamic> toJson() => <dynamic, dynamic>{
+        RemitDataKeys.files: files.map(fileToJson).toList(),
+        RemitDataKeys.folders: folders.map(folderToJson).toList(),
+      };
+
+  static _RouteDataFile fileFromJson(
+    final List<dynamic> x,
+  ) =>
+      (basename: x[0], size: x[1]);
+
+  static _RouteDataFolder folderFromJson(final List<dynamic> x) =>
+      (basename: x[0]);
+
+  static List<dynamic> fileToJson(final _RouteDataFile x) =>
+      <dynamic>[x.basename, x.size];
+
+  static List<dynamic> folderToJson(final _RouteDataFolder x) =>
+      <dynamic>[x.basename];
+}
 
 class RemitSenderServerFilesystemListRoute extends RemitSenderServerRoute {
   @override
@@ -67,7 +114,7 @@ class RemitSenderServerFilesystemListRoute extends RemitSenderServerRoute {
     );
   }
 
-  Future<RemitSenderServerFilesystemListData> makeRequest(
+  Future<RemitSenderServerFilesystemRouteData> makeRequest(
     final RemitReceiverConnection connection, {
     required final String path,
   }) async {
@@ -90,15 +137,16 @@ class RemitSenderServerFilesystemListRoute extends RemitSenderServerRoute {
     if (body.data == null) {
       throw RemitException.unexpectedResponseData();
     }
-    final Map<dynamic, dynamic>? data =
+    final Map<dynamic, dynamic>? raw =
         connection.optionalDecryptJsonOrNull(body.data!);
-    final List<String>? files = mapKeyAsListOrNull(data, RemitDataKeys.files);
-    final List<String>? folders =
-        mapKeyAsListOrNull(data, RemitDataKeys.folders);
-    if (files == null || folders == null) {
+    final RemitSenderServerFilesystemRouteData? data = jsonFactoryOrNull(
+      raw,
+      RemitSenderServerFilesystemRouteData.fromJson,
+    );
+    if (data == null) {
       throw RemitException.invalidResponseData();
     }
-    return (files: files, folders: folders);
+    return data;
   }
 
   static final RemitSenderServerFilesystemListRoute instance =
