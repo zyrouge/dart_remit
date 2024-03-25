a4d75b6b56248fda5aec58d260595527f34d9e81
zyrouge <zyrouge@hotmail.com>
2024-03-25 18:43:28 +0530

    chore: changelogs

--- lib/utils/eventer.dart
+++ lib/utils/eventer.dart
@@ -10,4 +10,10 @@ class RemitEventer<T> {
   void unsubscribe(final RemitEventListener<T> listener) {
     listeners.remove(listener);
   }
+
+  void dispatch(final T value) {
+    for (final RemitEventListener<T> x in listeners) {
+      x(value);
+    }
+  }
 }
